07b1b7cc51a5a530a6c7fe65bd15aab91db519f0
Alexis <alexis.carrizalesg@uanl.edu.mx>
2025-04-28 22:48:53 -0600

    Init Commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+
+def limpiarConsola():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+while(True):
+    print("PokeApi")
+
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+    print("6. ")
+    print("7. ")
+    print("8. ")
+    print("9. ")
+
+    print("10. Salir")
+
+    opc = input("Ingrese el numero de la opcion que desee consultar: ")
+
+    if(opc == "1"):
+        pass
+
+    elif(opc == "2"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "3"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "4"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "5"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "6"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "7"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "8"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "9"):
+        pass
+        limpiarConsola()
+
+    elif(opc == "10"):
+        break
+
+    else:
+        print("Elija una opcion valida...")
+        input("Precione cualquier tecla para continuar...")
+        limpiarConsola()
