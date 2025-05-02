8d46753288a9377946fa0cf49e1dfc00631af6b8
Alexis <alexis.carrizalesg@uanl.edu.mx>
2025-05-01 19:51:25 -0600

    Protopo de haibilidades y estaditicas

--- main.py
+++ main.py
@@ -5,69 +5,88 @@ close = False
 while(close != True):
     print("PokeApi")
     pokeSelect = input("Ingrese el pokemon que desea consultar: ")
+    
+    respuesta = requests.get(apiUrl + pokeSelect.lower())
 
-    while(close != True):
-         
-         print("1. Moves")
-         print("2. Abilities")
-         print("3. Pokémon (including various forms)")
-         print("4. Types")
-         print("5. Egg Groups")
-         print("6. Game Versions")
-         print("7. Items")
-         print("8. Pokédexes")
-         print("9. Pokémon Evolution Chains")
-         
-         print("10. Cambiar pokemon seleccionado" )
-         print("11. Salir")
-
-         opc = input("Ingrese el numero de la opcion que desee consultar: ")
-
-         if(opc == "1"):
-            pass
-
-         elif(opc == "2"):
-            pass
+    if(respuesta.status_code != 200):
+       print("Ingrese un valor valido...")
+       input("Pulse cualquier tecla para continuar")
+       limpiarConsola()
+    
+    else:
+      pokeData = respuesta.json()
+      while(close != True):
             limpiarConsola()
+            print(f"Pokedex de {pokeSelect}\n")
+            print("1. Estadisticas base")
+            print("2. Habilidades")
+            print("3. Moviminetos")
+            #print("4. ")
 
-         elif(opc == "3"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "4"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "5"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "6"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "7"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "8"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "9"):
-            pass
-            limpiarConsola()
-
-         elif(opc == "10"):
-            break
-         
-         elif(opc == "11"):
-            close = True
             
 
-         else:
-            print("Elija una opcion valida...")
-            input("Precione cualquier tecla para continuar...")
-            limpiarConsola()
-    
-    
+            """print("1. Moves")
+            print("2. Abilities")
+            print("3. Pokémon (including various forms)")
+            print("4. Types")
+            print("5. Egg Groups")
+            print("6. Game Versions")
+            print("7. Items")
+            print("8. Pokédexes")
+            print("9. Pokémon Evolution Chains")"""
+            
+            print("10. Cambiar pokemon seleccionado" )
+            print("11. Salir")
+
+            opc = input("Ingrese el numero de la opcion que desee consultar: ")
+
+            if(opc == "1"):
+               Estadisticas(pokeData)
+               limpiarConsola()
+              
+
+            elif(opc == "2"):
+               Habilidades(pokeData)
+               limpiarConsola()
+
+            elif(opc == "3"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "4"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "5"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "6"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "7"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "8"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "9"):
+               pass
+               limpiarConsola()
+
+            elif(opc == "10"):
+               break
+            
+            elif(opc == "11"):
+               close = True
+               
+
+            else:
+               print("Elija una opcion valida...")
+               input("Precione cualquier tecla para continuar...")
+               limpiarConsola()
+      
+      
--- utilities.py
+++ utilities.py
@@ -1,10 +1,31 @@
 import os
 import requests
+import json
+
+
 
 apiUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+
 
 def limpiarConsola():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
+
+
+def Estadisticas(pokemon):
+    for stat in pokemon['stats']:
+        nombre_stat = stat['stat']['name']
+        valor_stat = stat['base_stat']
+        print(f"{nombre_stat.capitalize()}:{valor_stat}")
+    print("")
+    input("Pulse cualquier tecla para continuar")   
+
+def Habilidades(pokemon):
+    for ability in pokemon["abilities"]:
+                print(ability["ability"]["name"])
+    print("")
+    input("Pulse cualquier tecla para continuar")
+    
